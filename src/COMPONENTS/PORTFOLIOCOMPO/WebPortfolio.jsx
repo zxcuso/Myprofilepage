20cd607d20df501460f1b1e70ffd137cb625e0b6
mrikharo <abduljelilikharo@yahoo.com>
2023-11-02 17:14:45 +0100

    mmm

--- src/COMPONENTS/PORTFOLIOCOMPO/WebPortfolio.jsx
+++ src/COMPONENTS/PORTFOLIOCOMPO/WebPortfolio.jsx
@@ -46,7 +46,45 @@ const WebPortfolio = () => {
                 </div>
 
                 </article>
+
+                <article className='webport__items'>
+                <div className='web__portimg'>
+                    <img src={IM3} alt="web page image" />
+                    
+                </div>
+                <div className='project__title'>
+                    <h3>Figma Dashboard UI kit for data</h3>
+                  
+
+                    <div className='description__web'>
+                        <h4>Project brief discription</h4>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui odio quae architecto provident deserunt soluta pariatur nam ipsum magnam, repellendus vitae nulla quasi dicta quisquam, vero, alias reprehenderit. Debitis, doloribus.
+                        </p>
+                    </div>
+                    
+                </div>
+                <hr className='hr__web'/>
+                <h5 className='lang__web'>Languages</h5>
+                <div className='web__language'>
+                    <div className='web__icons'><BiLogoReact size={20}/> <h5>React js</h5></div>
+                    <div className='web__icons'><AiFillHtml5 size={20}/> <h5>HTML</h5></div>
+                    <div className='web__icons'><DiCss3 size={20}/> <h5>CSS</h5></div>
+                </div>
+                <hr className='hr__web'/>
+
+                <div className='web__btn'>
+                    <div className='hr__webb'>
+                    <a href="https://github.com" className='btn but__web'><h5>Github</h5></a>
+                    </div>
+                    <a href="https://dribble.com" className='btn but__web'><h5>Live Demo</h5></a>
+                </div>
+
+                </article>
+                
             </div>
+
+            
         </section>
     )
 }
