6b9cdc4d54d984739c91afafc6c4b49d0f2bfb84
mrikharo <abduljelilikharo@yahoo.com>
2023-11-02 15:55:36 +0100

    update on portfolio section

--- src/COMPONENTS/ABOUNTCOMPONENT/Background.jsx
+++ src/COMPONENTS/ABOUNTCOMPONENT/Background.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css';
 
-const data = `Yunusa Abdulwahab holds a bachelor’s degree in accounting from Federal University Kashere, Gombe state. He is an associate member of the institute of chartered accountants of Nigeria (ICAN). 
+const data = `Yunusa Abdulwahab holds a bachelor's degree in accounting from Federal University Kashere, Gombe state. He is an associate member of the institute of chartered accountants of Nigeria (ICAN). 
 Till date, he works as Senior auditor with UF Idiagi & Co, a firm of chartered accountants with over six (6) years of field experience. He is also a lecturer at ICAN learning center Kaduna (Musa Bello Learning Center). 
 With huge interest in ICT and financial Technology.`
 
@@ -14,8 +14,8 @@ const Background = () => {
             </div>
             <div className="list__container">
                 <ul>
-                    <li>Nationality: <span>Nigeria</span></li>
-                    <li>State of Origin: <span>Edo State</span></li>
+                    <li>Nationality: <span>Nigeria.</span></li>
+                    <li>State of Origin: <span>Edo State.</span></li>
                     <li>Date of Birth: <span>December 20th, 1993.</span></li>
                 </ul>
             </div>
--- src/COMPONENTS/PORTFOLIOCOMPO/WebPortfolio.jsx
+++ src/COMPONENTS/PORTFOLIOCOMPO/WebPortfolio.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './Portfolio.css'
+import IM3 from '../../ASSETS/portfolio3.jpg';
+import {BiLogoReact} from 'react-icons/bi';
+import {AiFillHtml5} from 'react-icons/ai';
+import {DiCss3} from 'react-icons/di';
+
+
+
 
 const WebPortfolio = () => {
     return (
-        <div>WebPortfolio</div>
+        <section className='main__port-container_web'>
+            <div className='portfolio__content_web'>
+              <article className='webport__items'>
+                <div className='web__portimg'>
+                    <img src={IM3} alt="web page image" />
+                    
+                </div>
+                <div className='project__title'>
+                    <h3>Figma Dashboard UI kit for data</h3>
+                  
+
+                    <div className='description__web'>
+                        <h4>Project brief discription</h4>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui odio quae architecto provident deserunt soluta pariatur nam ipsum magnam, repellendus vitae nulla quasi dicta quisquam, vero, alias reprehenderit. Debitis, doloribus.
+                        </p>
+                    </div>
+                    
+                </div>
+                <hr className='hr__web'/>
+                <h5 className='lang__web'>Languages</h5>
+                <div className='web__language'>
+                    <div className='web__icons'><BiLogoReact size={20}/> <h5>React js</h5></div>
+                    <div className='web__icons'><AiFillHtml5 size={20}/> <h5>HTML</h5></div>
+                    <div className='web__icons'><DiCss3 size={20}/> <h5>CSS</h5></div>
+                </div>
+                <hr className='hr__web'/>
+
+                <div className='web__btn'>
+                    <div className='hr__webb'>
+                    <a href="https://github.com" className='btn but__web'><h5>Github</h5></a>
+                    </div>
+                    <a href="https://dribble.com" className='btn but__web'><h5>Live Demo</h5></a>
+                </div>
+
+                </article>
+            </div>
+        </section>
     )
 }
 
